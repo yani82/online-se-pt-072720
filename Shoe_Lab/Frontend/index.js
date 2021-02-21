9c21d0a8af9e2b3db9f72107acdec8d47dd9d15f
yani82 <yani.iman1@gmail.com>
2021-02-21 00:49:05 -0500

    returned a list of shoes

--- Shoe_Lab/Frontend/index.js
+++ Shoe_Lab/Frontend/index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+const listGroup = document.querySelector(".list-group")
+const shoesIndexUrl = "http://localhost:3000/shoes" 
+const mainShoeBox = document.querySelector("#main-shoe")
+let currentShoe = document.querySelector(".list-group-item")
+
+fetch(shoesIndexUrl)
+  .then(response => response.json())
+  .then((shoes) => {
+      let shoesHTML = shoes.map(function(shoe) {
+        return `<li class="list-group-item">${shoe.name}</li>`
+      })
+    listGroup.innerHTML += shoesHTML.join("")
+  });
+
+  
+
+
+
+
+
+
+
+
+
